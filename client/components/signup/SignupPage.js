7f2939fde5f2b63f2fdffc79014910d00b95cec3
frankNowinski <nowinski.frank@gmail.com>
2016-09-11 00:24:16 -0400

    added signup page

---
+++ client/components/signup/SignupPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignupPage extends Component {
+  render() {
+    return (
+      <div>Sign up</div>
+    )
+  }
+}
+
+export default SignupPage;
---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  entry: [
+    path.join(__dirname, '/client/index.js')
+  ],
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ],
+  output: {
+    path: '/',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: ['babel']
+      }
+    ]
+  },
+  resolve: {
+    extentions: ['', '.js']
+  },
+  devtools: 'eval-source-map'
+}
