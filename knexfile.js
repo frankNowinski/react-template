7210c3e54f1eb0b72285177e65e445ddee44bc13
frankNowinski <nowinski.frank@gmail.com>
2016-09-12 00:57:21 -0400

    added postgres

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'rem',
+      user:     'Frank',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
